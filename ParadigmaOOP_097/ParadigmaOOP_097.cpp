183e9b89700d422f33265097d52cb07108395189
farisabqari <127277279+farisabqari@users.noreply.github.com>
2023-05-24 12:24:35 +0800

    int main

--- ParadigmaOOP_097/ParadigmaOOP_097.cpp
+++ ParadigmaOOP_097/ParadigmaOOP_097.cpp
@@ -36,4 +36,11 @@ public:
 	}
 };
 
+int main() {
+	pelajar siswa1("andi laksono", "SMA 1 Bantul");
+	cout << siswa1.perkenalan();
+	cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+	return 0;
+}
+
 
