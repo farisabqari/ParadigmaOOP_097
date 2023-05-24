6ccd7095594105e84f7d8630e4d820346ffd06a0
farisabqari <127277279+farisabqari@users.noreply.github.com>
2023-05-24 18:57:12 +0800

    memasukan id,nama,dan nilai

--- memberinitlist.cpp/memberinitlist.cpp.cpp
+++ memberinitlist.cpp/memberinitlist.cpp.cpp
@@ -8,5 +8,14 @@ private:
 	string nama;
 	float nilai;
 
+public:
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) { //member initialization list
+	 //definisi
+	}
+	~mahasiswa(){
+		cout << "id    =" << id << endl;
+	    cout << "nama  =" << nama << endl;
+		cout << "nilai =" << nilai << endl;
+	}
 
 };
